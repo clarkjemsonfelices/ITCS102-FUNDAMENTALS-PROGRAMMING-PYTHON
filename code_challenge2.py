1c1854597254adc7a15132df271efc19c9f10a3e
clarkjemsonfelices <felicesclarkjemson0726@gmail.com>
2025-08-15 15:53:14 +0800

    Add files via upload

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,39 +1,39 @@
 amount = eval(input("Enter amount to deposit: ")) 
-print("Here is a breakdown, using PH denomination:") 
+
 #1000
-rk = amount / 1000 // 1 * 1000
-k = rk / 1000
+p1000 = amount  // 1000
+amount = amount % 1000
 #500
-rfh = (amount - rk) / 500 // 1 * 500
-fh = rfh / 500
+p500 = amount // 500
+amount = amount % 500
 #200
-rth = (amount - (rk + rfh)) / 200 // 1 * 200
-th = rth / 200
+p200 = amount // 200
+amount = amount % 200
 #100
-rh = (amount - (rk + rfh + rth)) / 100 // 1 * 100
-h = rh / 100
+p100 = amount // 100
+amount = amount % 100
 #50
-rft = (amount - (rk + rfh + rth + rh)) / 50 // 1 * 50
-ft = rft / 50
+p50 = amount // 50
+amount = amount % 50
 #20
-rtt = (amount - (rk + rfh + rth + rh + rft)) / 20 // 1 * 20
-tt = rtt / 20
+p20 = amount // 20
+amount = amount % 20
 #10
-rt = (amount - (rk + rfh + rth + rh + rft)) / 10 // 1 * 10
-t = rt / 10
+p10 = amount // 10
+amount = amount % 10
 #5
-rf = (amount - (rk + rfh +rth + rh + rft + rt)) / 5 // 1 * 5
-f = rf / 5
+p5 = amount // 5
+amount = amount % 5
 #1
-o = (amount - (rk + rfh +rth + rh + rft +rt + rf)) // 1
+p1 = amount // 1
 
-
-print("1000 -", k)
-print("500 -", fh)
-print("200 -", th)
-print("100 -", h) 
-print("50 -", ft) 
-print("20 -", tt) 
-print("10 -", t)
-print("5 -", f) 
-print("1 -", o) 
+print("Here is a breakdown, using PH denomination:") 
+print("1000 -", p1000)
+print("500 -", p500)
+print("200 -", p200)
+print("100 -", p100) 
+print("50 -", p50) 
+print("20 -", p20) 
+print("10 -", p10)
+print("5 -", p5) 
+print("1 -", p1) 
