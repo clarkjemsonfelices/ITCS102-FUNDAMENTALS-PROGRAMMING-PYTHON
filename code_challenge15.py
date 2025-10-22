2be03f299662a0a9b69509b05e17185b119b32d7
clarkjemsonfelices <felicesclarkjemson0726@gmail.com>
2025-10-22 15:09:17 +0800

    Add files via upload

--- code_challenge15.py
+++ code_challenge15.py
@@ -1,11 +1,12 @@
 anime = []
 while True:
-	title = input("Enter the title of an anime (or type 'exit to finish): ")
+	title = input("Enter the title of an anime (or type 'exit' to finish): ")
 	
 	if title.lower() == 'exit':
 		print("You have exited the the anime entry program.")
 		break
 	else:
+		print(f"'{title}' has been added to your anime list.")
 		anime.append(title)
 		continue
 print("Your anime list includes:")
